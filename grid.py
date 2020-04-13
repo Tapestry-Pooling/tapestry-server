68a51ad2b7ae40c98c72ad2ca7f5190bb103ae96
Raunak <raunak.ramakrishnan@shop101.com>
2020-04-13 18:39:35 +0530

    Adding API to list distinct cells for grid

--- grid.py
+++ grid.py
@@ -1,5 +1,5 @@
 import itertools as it
-import json
+import orjson
 import numpy as np
 import string
 
@@ -12,4 +12,22 @@ def generate_grid(a):
     for r in aa:
         ones = np.nonzero(r)[0] * 2
         l.append({ "screenData" : [C[i] for i in ones]})
-    return json.dumps({"gridData" : l})
+    return {"gridData" : l}
+
+def generate_grid_json(a):
+    return orjson.dumps(generate_grid(a))
+
+def cells_from_grid(a):
+    g = generate_grid(a)["gridData"]
+    cells = []
+    cc = set()
+    for r in g:
+        h = r["screenData"]
+        for c in h:
+            if c not in cc:
+                cells.append(c)
+                cc.add(c)
+    return { "cellData" : cells }
+
+def cells_from_grid_json(a):
+    return orjson.dumps(cells_from_grid(a))
