764238333b303ecc786d9da5fd0fe16177a40a55
Raunak <raunak.ramakrishnan@shop101.com>
2020-04-17 20:21:13 +0530

    Added simple numbering scheme for grid if num_tests > 46

--- grid.py
+++ grid.py
@@ -6,7 +6,7 @@ import string
 
 # 8 x 12 array
 C = [f'{t[0]}{t[1]}' for t in it.product(string.ascii_uppercase,range(1,13))]
-BATCH_REGEX = r'(\d+)x(\d+).+'
+BATCH_REGEX = r'(\d+)x(\d+).*'
 
 def generate_simple_grid(a):
     print("Using simple grid scheme")
@@ -37,17 +37,20 @@ def cells_from_grid(g):
 def cells_from_grid_json(g):
     return orjson.dumps(cells_from_grid(g))
 
+def generate_grid_and_cell_data(batch, a):
+    m, _ = parse_batch(batch)
+    g = generate_grid(a) if m < 48 else generate_simple_grid(a)
+    return g, cells_from_grid(g)
+
 def generate_grid_and_cell_data_json(mlabels, matrices):
     grid_dict = {}
     cell_dict = {}
     for batch in mlabels:
-        # TODO : Decide which algo to use based on batch size
         label = mlabels[batch]
-        m, n = parse_batch(batch)
         if label in matrices:
             a = matrices[label]
-            g = generate_grid(a) if m < 48 else generate_simple_grid(a)
-            cell_dict[batch] = cells_from_grid_json(g)
+            g, c = generate_grid_and_cell_data(batch, a)
+            cell_dict[batch] = orjson.dumps(c)
             grid_dict[batch] = orjson.dumps(g)
         else:
             cell_dict[batch] = '{}'
