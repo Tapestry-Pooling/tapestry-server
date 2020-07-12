e80a455b55b85f142467b6a56a27f01054bad75a
Aakriti <aakriti281020@gmail.com>
2020-07-13 03:41:04 +0530

    Switched database

--- pooling/settings/dev.py
+++ pooling/settings/dev.py
@@ -11,7 +11,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pooling',
+        'USER': 'postgres',
+        'PASSWORD': 'password1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
