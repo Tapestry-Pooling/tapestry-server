a037112955854e7b8c62bf2d821db90a37ea34d8
ROHIT KUMAR <rohitkm@cse.iitb.ac.in>
2020-07-16 17:44:56 +0530

    email-id auth and custom user fix

--- rest/views/user.py
+++ rest/views/user.py
@@ -3,6 +3,6 @@ from rest.models import User
 from rest.serializers import UserSerializer
 
 
-class LabViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
