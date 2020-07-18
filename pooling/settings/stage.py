3f512fa63bb4b8dbc94d589aa876817cbc4c0b2b
Nilesh Bhosale <nileshbhosale215@gmail.com>
2020-07-18 20:55:31 +0530

    Added allowed host for appengine

--- pooling/settings/stage.py
+++ pooling/settings/stage.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','tapestry-pooling.el.r.appspot.com']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
