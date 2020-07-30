1b1b3597ada8ec7dd6196c983f1e8d278870e935
ROHIT KUMAR <rohitkm@cse.iitb.ac.in>
2020-07-30 22:31:43 +0530

    api integration for generating pooling-matrix

--- rest/serializers/test.py
+++ rest/serializers/test.py
@@ -4,5 +4,4 @@ from rest.models import Test
 class TestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Test
-        fields = ['id', 'assigned_to', 'status', 'samples', 'suspected_samples', 'positive_samples', 'test_kit', 'machine_type', 'remark', 'file', 'pooling_matrix', 'mastermix_matrix', 'patient_id_map', 'results_1', 'results_2']
-
+        fields = ['id', 'assigned_to', 'status', 'samples', 'suspected_samples', 'positive_samples', 'test_kit', 'machine_type', 'remark', 'file', 'pooling_matrix', 'mastermix_matrix', 'patient_id_map', 'results_1', 'results_2', 'prevalence']
