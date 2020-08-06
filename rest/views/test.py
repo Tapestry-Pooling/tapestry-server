a63656f6858e3cc2fa962bf227d7ae8e847786ab
ROHIT KUMAR <rohitkm@cse.iitb.ac.in>
2020-08-06 09:06:44 +0530

    user get api and fixed test filter

--- rest/views/test.py
+++ rest/views/test.py
@@ -2,14 +2,15 @@ from rest_framework import viewsets
 from rest.models import Test
 from rest.serializers import TestSerializer
 from rest_framework_json_api import django_filters
-from rest_framework import filters
+from rest_framework_json_api import filters
+from rest_framework.filters import SearchFilter
 
 
 class TestViewSet(viewsets.ModelViewSet):
     queryset = Test.objects.all()
     serializer_class = TestSerializer
-    filter_backends = [django_filters.DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ['status', ]
+    filter_backends =(filters.QueryParameterValidationFilter, django_filters.DjangoFilterBackend, SearchFilter)
+    filterset_fields = {'status': ('exact', 'in'),}
     search_fields = ['id', ]
 
     def get_queryset(self):
