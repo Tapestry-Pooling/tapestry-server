0a5e2cb43fb46ac03b6bf0e645ae373a8f451476
ROHIT KUMAR <rohitkm@cse.iitb.ac.in>
2020-08-13 22:23:04 +0530

    validation for activate user and extra columns for lab config added

--- rest/admin/user.py
+++ rest/admin/user.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from rest.forms import CustomUserCreationForm, CustomUserChangeForm
-from rest.models import User
+from rest.models import User, LabConfiguration
 from django.utils.html import format_html
 from django.urls import reverse
 from django.conf.urls import url
@@ -11,12 +11,14 @@ from allauth.account.utils import send_email_confirmation
 from django.contrib.auth.forms import PasswordResetForm
 from pooling import settings
 from allauth.account.models import EmailAddress
+from django.contrib import messages
+
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = User
-    list_display = ('email', 'is_staff', 'is_active', 'designation', 'lab_id','activate')
+    list_display = ('email', 'is_staff', 'is_active', 'designation', 'lab_id', 'is_lab_config_added', 'activate')
     list_filter = ('email', 'is_staff', 'is_active',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
@@ -30,7 +32,7 @@ class CustomUserAdmin(UserAdmin):
     )
     search_fields = ('email',)
     ordering = ('email',)
-    
+
     def activate(self, obj):
         if obj.is_staff:
             return None
@@ -38,23 +40,33 @@ class CustomUserAdmin(UserAdmin):
         email_address = EmailAddress.objects.get(email=obj.email)
         if email_address.verified:
             return None
-        
+
         return format_html(
             '<a class="button" href="{}">Activate User</a>&nbsp;',
             reverse('admin:activate-user', args=[obj.pk]),
         )
+
     activate.short_description = 'Activate User'
     activate.allow_tags = True
 
     def handle_activate_user(self, request, id, *args, **kwargs):
 
-        user = User.objects.get(pk= id)
+        user = User.objects.get(pk=id)
+
+        # check if lab configuration is added
+        if not LabConfiguration.objects.filter(lab_id=user.lab_id).exists():
+            messages.error(
+                request,
+                "Lab configuration is not added!"
+            )
+            return HttpResponseRedirect("../../")
+
         user.is_active = True
         # user.password = get_random_string(length=32)
         user.save(force_update=True)
 
         # send_email_confirmation(request,user, False)
-        reset_form = PasswordResetForm(data={'email':user.email})
+        reset_form = PasswordResetForm(data={'email': user.email})
         reset_form.is_valid()
 
         opts = {
@@ -65,7 +77,12 @@ class CustomUserAdmin(UserAdmin):
             'email_template_name': 'registration/set_password.html'
         }
         reset_form.save(**opts)
-        
+
+        messages.info(
+            request,
+            "{} has been activated!".format(user.email)
+        )
+
         return HttpResponseRedirect("../../")
 
     def get_urls(self):
@@ -77,6 +94,16 @@ class CustomUserAdmin(UserAdmin):
                 name='activate-user'),
         ]
         return custom_urls + urls
+
+    def is_lab_config_added(self, obj):
+        if obj.is_staff:
+            return True
+        if LabConfiguration.objects.filter(lab_id=obj.lab_id).exists():
+            return True
+        return False
+
+    is_lab_config_added.boolean = True
+    is_lab_config_added.short_description = "Lab Configuration Added"
 
 
 admin.site.register(User, CustomUserAdmin)
--- rest/models/lab_configuration.py
+++ rest/models/lab_configuration.py
@@ -1,11 +1,13 @@
 from django.db import models
 from .machine_type import MachineType
 from .test_kit import TestKit
+from .lab import Lab
 
 class LabConfiguration(models.Model) :
-	lab_id = models.SmallIntegerField()
+	lab_id = models.ForeignKey(
+		Lab,
+		on_delete=models.CASCADE
+	)
 	machine_type = models.ManyToManyField(MachineType)
 	test_kit = models.ManyToManyField(TestKit)
 	resource_name = 'labConfiguration'
-
-
