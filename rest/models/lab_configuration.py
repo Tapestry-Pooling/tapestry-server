081aba62b7cb9f7b6a678a3f01625e890d66ad91
Akanksha Vyas <vyasakanksha@gmail.com>
2020-08-24 20:44:05 +0530

    added max poolsize

--- rest/models/lab_configuration.py
+++ rest/models/lab_configuration.py
@@ -11,6 +11,7 @@ class LabConfiguration(models.Model) :
 	machine_type = models.ManyToManyField(MachineType)
 	test_kit = models.ManyToManyField(TestKit)
 	resource_name = 'labConfiguration'
-    nsamples = models.SmallIntegerField()
-    prevalence = models.FloatField()
-    poolingmatrix_filename = models.TextField(blank=True)
+	nsamples = models.SmallIntegerField()
+	prevalence = models.FloatField()
+	max_poolsize = models.FloatField()
+	poolingmatrix_filename = models.TextField(blank=True)
