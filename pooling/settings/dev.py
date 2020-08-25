11b2adc08f6f76847d454e0e23453c7de612ce7f
Nilesh Bhosale <nileshbhosale215@gmail.com>
2020-08-25 20:43:22 +0530

    Update dev.py

--- pooling/settings/dev.py
+++ pooling/settings/dev.py
@@ -3,8 +3,8 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-SENDGRID_SANDBOX_MODE_IN_DEBUG = True
-SENDGRID_ECHO_TO_STDOUT=True
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
+SENDGRID_ECHO_TO_STDOUT=False
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
