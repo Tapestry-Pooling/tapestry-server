0ef82c830b7539215a1b034246a8b79066ef0d39
ROHIT KUMAR <rohitkm@cse.iitb.ac.in>
2020-09-08 00:13:37 +0530

    update test model and serializer

--- rest/serializers/test.py
+++ rest/serializers/test.py
@@ -6,4 +6,5 @@ class TestSerializer(serializers.ModelSerializer):
     pooling_matrix_download_url = serializers.ReadOnlyField()
     class Meta:
         model = Test
-        fields = '__all__'
+        exclude = ['poolingmatrix_filename', 'poolingscheme_filename', 'testctresults_filename', 'report_filename']
+        read_only_fields = ['status', 'npositive', 'ninconclusive', 'nnegative', 'positive', 'negative', 'inconclusive']
