8bf5811e0a96e1f5d98410361e68a8c2718c30fa
ROHIT KUMAR <rohitkm@cse.iitb.ac.in>
2020-09-28 11:16:59 +0530

    send email to user after admin verify test result

--- rest/util/user_alert.py
+++ rest/util/user_alert.py
@@ -27,3 +27,24 @@ def new_user_alert(user):
         print("Error")
         print(exp)
         logger.error('Error sending new user alert to user')
+
+
+def test_result_alert_user(user, positive, inconclusive):
+    logger = logging.getLogger(__name__)
+    try:
+        subject = render_to_string('registration/results_email_subject.txt')
+        html_body = render_to_string(
+            'registration/results_email.html', {'user': user, 'positive': positive, "inconclusive": inconclusive}
+        )
+        send_mail(
+            subject=subject,
+            message="",
+            html_message=html_body,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+            fail_silently=False,
+        )
+    except Exception as exp:
+        print("Error")
+        print(exp)
+        logger.error('Error sending test result alert to user')
