865545cc0f217b2a6900bcc5505de41f90ddc30f
ROHIT KUMAR <rohitkm@cse.iitb.ac.in>
2020-10-05 15:01:40 +0530

    prod corrected url

--- pooling/settings/prod.py
+++ pooling/settings/prod.py
@@ -22,5 +22,5 @@ DATABASES = {
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://tapestry-pooling-f9f8e.web.app/"
+    "https://tapestry-pooling-f9f8e.web.app"
 ]
