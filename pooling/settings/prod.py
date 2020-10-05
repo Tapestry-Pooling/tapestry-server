75628ee56c3dae5dd4628f47113b0c5a0e6b3b97
ROHIT KUMAR <rohitkm@cse.iitb.ac.in>
2020-10-05 14:45:18 +0530

    add cors origin whitelist in prod

--- pooling/settings/prod.py
+++ pooling/settings/prod.py
@@ -17,3 +17,10 @@ DATABASES = {
         'PORT': os.environ['DB_PORT'],
     }
 }
+
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://tapestry-pooling-f9f8e.web.app/"
+]
